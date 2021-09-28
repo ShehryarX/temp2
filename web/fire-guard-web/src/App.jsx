d3050be25801b6caaa217f585847daa468c664c8
tpinto7 <tjpinto@uwaterloo.ca>
2021-09-28 04:02:33 -0400

    clusters kinda working

--- web/fire-guard-web/src/App.jsx
+++ web/fire-guard-web/src/App.jsx
@@ -4,14 +4,26 @@ import React from 'react';
 import { Card, Layout } from 'antd';
 import Map from './Map';
 import Map2 from './Map2';
+import MapGL, {Source, Layer} from 'react-map-gl';
 
 const { Header, Footer, Sider, Content } = Layout;
+const MAPBOX_TOKEN = 'pk.eyJ1IjoidHBpbnRvNyIsImEiOiJja2JicWYwMzkwM3NnMnNtZnZkbXU5dGhkIn0.NdzHwoMYvZ-fSTIA9xXXfw';
 
 export class App extends React.Component {
+  state = { 
+    viewport: { 
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    }
+
+  }
   render() {
+    const {viewport} = this.state;
+    console.log("testsetsetet")
     return (
       <div>
-        <Layout style={{ minHeight: '100vh' }}>
+        <Layout>
           <Header>
             <div style={{ color: 'white' }}>
               Fire Guardian
@@ -27,9 +39,20 @@ export class App extends React.Component {
                 <p>Card content</p>
               </Card>
             </div> */}
-
-            <Map style={ {width: '100%', height: '100%', position: 'relative' }}/>
-            {/* <Map2 style={ {width: '100%', height: '100%', position: 'relative' }}/> */}
+            {/* <div className="tyler"> 
+              <Map style={ {width: '100%', height: '100%', position: 'relative' }}/>
+            </div>  */}
+            {/* <div className="hussain" width="1000px" height="1000px" position="absolute" top="100px"> 
+              <MapGL
+                  {...viewport}
+                  width="500px"
+                  height="500px"
+                  onViewportChange={(viewport) => this.setState({viewport})}
+                  mapboxApiAccessToken={MAPBOX_TOKEN}
+                  visible={true}
+              />
+            </div> */}
+            <Map2 width="500px" height="500px"/>
 
           {/* </Content> */}
         </Layout>
--- web/fire-guard-web/src/Map2.jsx
+++ web/fire-guard-web/src/Map2.jsx
@@ -1,8 +1,10 @@
    
 import * as React from 'react';
-import {useState, useRef} from 'react';
+import {useState, useRef, useEffect, useCallback} from 'react';
 import {render} from 'react-dom';
 import MapGL, {Source, Layer} from 'react-map-gl';
+import { Modal, List } from 'antd';
+
 
 import ControlPanel from './control-panel-2';
 import {clusterLayer, clusterCountLayer, unclusteredPointLayer} from './layers';
@@ -20,37 +22,147 @@ export default function Map2() {
   });
   const mapRef = useRef(null);
 
-  const onClick = event => {
-    const feature = event.features[0];
-    const clusterId = feature.properties.cluster_id;
-
-    const mapboxSource = mapRef.current.getMap().getSource('earthquakes');
-
-    mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
-      if (err) {
-        return;
-      }
-
-      setViewport({
-        ...viewport,
-        longitude: feature.geometry.coordinates[0],
-        latitude: feature.geometry.coordinates[1],
-        zoom,
-        transitionDuration: 500
-      });
-    });
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  // const [features, setFeatures] = useState([]);
+  const [leaves, setLeaves] = useState([]);
+  const showModal = () => {
+    setIsModalVisible(true);
   };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    setLeaves([]);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    setLeaves([]);
+  };
+
+  // const handleContextMenu = (e) => { 
+  //   console.log("starting")
+  //   showModal();
+  //   e.preventDefault();
+  //     console.log("hussf")
+  //     console.log(e);
+  //   if(e.features) { 
+  //     console.log(e.features);
+  //   }
+  //   console.log(e.features)
+  //   setFeatures(e.features);
+  //   // setXPos(`${e.pageX}px`);
+  //   // setYPos(`${e.pageY}px`);
+  //   showModal();
+  // }
+
+  // const handleContextMenu = useCallback(
+  //   (e) => {
+  //     console.log("starting")
+  //     showModal();
+  //     e.preventDefault();
+  //       console.log("hussf")
+  //       console.log(e);
+  //     if(e.features) { 
+  //       console.log(e.features);
+  //     }
+  //     console.log(e.features)
+      
+  //     // setXPos(`${e.pageX}px`);
+  //     // setYPos(`${e.pageY}px`);
+  //     showModal();
+  //   },
+  //   []
+  // );
+
+  const handleClick = useCallback((event) => {
+
+    if(!isModalVisible && event.features && event.features[0].properties) {
+      console.log("testsetste")
+      console.log(isModalVisible)
+      const feature = event.features[0]; 
+        const clusterId = feature.properties.cluster_id;
+        console.log(clusterId)
+        const mapboxSource = mapRef.current.getMap().getSource('earthquakes');
+        if(feature.layer.id === "clusters") { 
+            mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+                if (err) {
+                    return;
+                }
+                setViewport({
+                    ...viewport,
+                    longitude: feature.geometry.coordinates[0],
+                    latitude: feature.geometry.coordinates[1],
+                    zoom,
+                    transitionDuration: 500
+                });
+            });
+        } else if(feature.layer.id == "unclustered-point"){ 
+
+          setViewport({
+              ...viewport,
+              longitude: feature.geometry.coordinates[0],
+              latitude: feature.geometry.coordinates[1],
+              transitionDuration: 500
+          });
+
+        } else { 
+
+            setViewport({
+                ...viewport,
+                longitude: feature.geometry.coordinates[0],
+                latitude: feature.geometry.coordinates[1],
+                transitionDuration: 500
+            });
+        }
+    }
+
+  }, [isModalVisible, setViewport]);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    // document.addEventListener("contextmenu", handleContextMenu);
+    return () => {
+      document.addEventListener("click", handleClick);
+      // document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  });
+
+  const onClick = (e) => { 
+    if(e.features) { 
+      const feature = e.features[0];
+      var clusterId = feature.properties.cluster_id,
+      point_count = feature.properties.point_count,
+      clusterSource = mapRef.current.getMap().getSource(/* cluster layer data source id */'earthquakes');
+     
+      // Get all points under a cluster
+      // let leaves;
+      if(feature.layer.id === "clusters") {  
+        clusterSource.getClusterLeaves(clusterId, point_count, 0, function(err, aFeatures){
+          if(err) {
+            console.log(err);
+            return;
+          }
+          setLeaves(aFeatures);
+          console.log(aFeatures)
+        });
+        setIsModalVisible(true);
+      } else { 
+        // TODO: set to shehryar's model for single dashboard
+        setIsModalVisible(true);
+      }
+    }
+  }
 
   return (
     <>
       <MapGL
         {...viewport}
-        width="100%"
-        height="100%"
+        width="1000px"
+        height="500px"
         mapStyle="mapbox://styles/mapbox/dark-v9"
         onViewportChange={setViewport}
         mapboxApiAccessToken={MAPBOX_TOKEN}
-        interactiveLayerIds={[clusterLayer.id]}
+        interactiveLayerIds={[clusterLayer.id, unclusteredPointLayer.id]}
         onClick={onClick}
         ref={mapRef}
       >
@@ -67,6 +179,16 @@ export default function Map2() {
           <Layer {...unclusteredPointLayer} />
         </Source>
       </MapGL>
+      <Modal title="Cluster's beacons" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <List
+          dataSource={leaves}
+          renderItem={item => (
+            <List.Item>{item.properties.id}</List.Item>
+          )}
+
+        />
+     
+      </Modal>
       <ControlPanel />
     </>
   );
